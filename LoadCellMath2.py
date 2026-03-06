2e9cf9fe1b8f1ac2f42aa986dfaa164f5826c4db
npat2227 <22npatel07@gmail.com>
2026-03-06 12:27:19 -0500

    changed filter to check pressure > 10 rather than thrust > input upper thrust

--- LoadCellMath2.py
+++ LoadCellMath2.py
@@ -51,7 +51,7 @@ class Load_cell_math:
 
     # Getting Data
     # Extract time (first column), thrust (third column), and pressure (fifth column) data
-    dataTable = dataTable[dataTable.iloc[:,2] > self.upperthrust]
+    dataTable = dataTable[dataTable.iloc[:,4] > 10]
     dataTable.reset_index(drop=True)
 
     time_ms = dataTable.iloc[:, 0].values 
